5a4a7176d184c7473e8d7ef5f53ded1ba5784401
arjun225 <p.arjuncrm@gmail.com>
2022-01-13 21:57:40 +0530

    commited

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "eks-terraform-state-statging"
+    bucket = "terraform-state-malli"
     key    = "eks/terraform2.tfstate"
     region = "us-west-2"
   }
@@ -78,44 +78,44 @@ output "jenkins_ip_address" {
 
 
 
-module "eks" {
-  source = "terraform-aws-modules/eks/aws"
+# module "eks" {
+#   source = "terraform-aws-modules/eks/aws"
 
-  cluster_name                    = "my-cluster-demo"
-  cluster_version                 = "1.21"
-  cluster_endpoint_private_access = true
-  cluster_endpoint_public_access  = true
+#   cluster_name                    = "my-cluster-demo"
+#   cluster_version                 = "1.21"
+#   cluster_endpoint_private_access = true
+#   cluster_endpoint_public_access  = true
 
-  cluster_addons = {
-    coredns = {
-      resolve_conflicts = "OVERWRITE"
-    }
-    kube-proxy = {}
-    vpc-cni = {
-      resolve_conflicts = "OVERWRITE"
-    }
-  }
+#   cluster_addons = {
+#     coredns = {
+#       resolve_conflicts = "OVERWRITE"
+#     }
+#     kube-proxy = {}
+#     vpc-cni = {
+#       resolve_conflicts = "OVERWRITE"
+#     }
+#   }
 
 
-  vpc_id     = "vpc-0072899470bf72f18"
-  subnet_ids = ["subnet-0a6e7183b4dccda71", "subnet-0a5b7efb61e20c248"]
+#   vpc_id     = "vpc-0072899470bf72f18"
+#   subnet_ids = ["subnet-0a6e7183b4dccda71", "subnet-0a5b7efb61e20c248"]
 
-#########################
-  eks_managed_node_groups = {
-    blue = {}
-    green = {
-      min_size     = 1
-      max_size     = 1
-      desired_size = 1
+# #########################
+#   eks_managed_node_groups = {
+#     blue = {}
+#     green = {
+#       min_size     = 1
+#       max_size     = 1
+#       desired_size = 1
 
-      instance_types = ["t3.medium"]
-      capacity_type  = "SPOT"
-      subnet         = "subnet-0a6e7183b4dccda71"
-      labels = {
-        Environment = "test"
-        GithubRepo  = "terraform-aws-eks"
-        GithubOrg   = "terraform-aws-modules"
-      }
-    }
-  }
-}
+#       instance_types = ["t3.medium"]
+#       capacity_type  = "SPOT"
+#       subnet         = "subnet-0a6e7183b4dccda71"
+#       labels = {
+#         Environment = "test"
+#         GithubRepo  = "terraform-aws-eks"
+#         GithubOrg   = "terraform-aws-modules"
+#       }
+#     }
+#   }
+# }
